222d1b88cbc78f6d4092c1f81cc5a8d0f66ee7e2
limipp <limipop@gmail.com>
2020-06-23 11:39:47 +0300

    version 3

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload Part 2...
+          Edit <code>src/App.js</code> and save to reload Part 3...
         </p>
         <a
           className="App-link"
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React fast fast fast
+          Learn React fast and secure
         </a>
       </header>
     </div>
